7d5bd25939619332ef292111f1b8c31f8ef7d513
Ajay054 <ajaykumar.gurram@informationtechconsultents.co.uk>
2024-08-28 15:55:08 +0100

    jar

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,10 +1,13 @@
 package org.ajay.com
 
-object CountNumbers {
+object WordCount {
   def main(args: Array[String]): Unit = {
-    val limit = 10 // Change this to any number you want to count up to
-    for (i <- 1 to limit) {
-      println(i)
-    }
+    val text = "This is a simple word count program in Scala." // Input text
+    val wordCount = countWords(text)
+    println(s"Number of words: $wordCount")
+  }
+
+  def countWords(text: String): Int = {
+    text.split("\\s+").length // Split the string by whitespace and count the words
   }
 }
